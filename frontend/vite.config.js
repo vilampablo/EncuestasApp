4a2f54fdaa9061a8733469c3544b7120a348bc45
vilampablo <vilam.pablo@gmail.com>
2024-08-16 12:19:39 -0600

    Config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  logLevel: 'info', // Set to 'info' or 'debug' for more detailed output
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+});
