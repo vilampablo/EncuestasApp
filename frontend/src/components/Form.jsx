93bea08c9d2b1c99ad1286506746109090556db6
vilampablo <vilam.pablo@gmail.com>
2024-08-16 12:42:23 -0600

    react-router-dom import statement fixes

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import api from '../api'
-import { useNavigate } from 'react-router-dom'
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
-import LoadingIndicator from './LoadingIndicator'
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({route, method}) {
     const [username, setUsername] = useState("")
