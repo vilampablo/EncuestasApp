af01ef300d61f9ee1e427af11b0a4f337da95e29
vilampablo <vilam.pablo@gmail.com>
2024-08-16 12:07:30 -0600

    pachage dependency was missing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  logLevel: 'info', // Set to 'info' or 'debug' for more detailed output
 })
