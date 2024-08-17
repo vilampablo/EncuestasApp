5a9ae67effaebca26b90302b3202e703bbab5036
vilampablo <vilam.pablo@gmail.com>
2024-08-17 02:29:23 -0600

    vite.config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'react-router-dom': path.resolve(__dirname, './node_modules/react-router-dom')
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
     }
   }
 })
